d0b87dee7920eb6d2a3b3434c3bdae765d44a679
Zebra!!! <zzz843702124@gmail.com>
2024-02-14 16:18:40 -0500

    work done

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,16 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Form from "@/pages/qcomps/stuckForm";
+import FeedbackForm from "@/pages/qcomps/thankYouCrash";
+import Counter from "@/pages/components/snapshot";
+import RequestTracker from "@/pages/qcomps/shoppingCart";
+import Scoreboard from "@/pages/qcomps/updObjectsForm";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Scoreboard />
     </div>
   )
 }
